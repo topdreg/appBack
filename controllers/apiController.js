5734a616d1e685c8001d871ed94f566a70f1806f
topdreg <apercival314@gmail.com>
2018-07-30 15:32:02 -0700

    added more stuff

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,73 @@
 var fs = require('fs');
+var sanitize = require('mongo-sanitize');
 var User = require('../models/user');
 var Image = require('../models/img');
+
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
+exports.signup_post = function(req, res) {
+
+	if (req.body.login.length < 6 || req.body.password.length < 6) {
+			res.json({
+				confirmation: "fail",
+				message: "Username and password needs at least 6 characters."
+			})
+	}
+
+	cleanName = sanitize(req.body.login);
+	cleanPassword = sanitize(req.body.password);
+
+	var user = new User(
+		{
+			username: cleanName,
+			password: cleanPassword
+		}
+	);
+	User.findOne({ 'username': req.body.login })
+		.exec( function(err, found_user) {
+			if (err) { console.log("Error occurred!"); }
+
+			if (found_user) { console.log("Username already taken."); }
+
+			else {
+				user.save();
+			}
+		})
+		res.json({
+			confirmation: "success"
+		})
+};
+
+exports.login_post = function(req, res) {
+	
+	cleanName = sanitize(req.body.login);
+	cleanPassword = sanitize(req.body.password);
+
+	User.findOne({ 'username': cleanName })
+		.exec( function(err, found_user) {
+			if (err) { console.log("Error occurred!"); }
+
+			if (found_user) {  
+				if (cleanPassword === found_user.password) {
+					res.json({
+						confirmation: 'success',
+						data: found_user
+					})
+				}
+				else {
+					res.json({
+						confirmation: 'fail'
+					})
+				}
+			}
+
+			else {
+				console.log("Username is not in the system - please register");
+			}
+		})
+	return;
+};
 
 exports.resource_get = function(req, res) {
 	const { resource } = req.params
@@ -23,8 +90,7 @@ exports.resource_get = function(req, res) {
 };
 
 exports.image_post = function(req, res) {
-	const userId = req.params.id;
-	var image = new Image;
+	const userId = req.params.id; var image = new Image;
 	image.img = req.body.base64;
 	image.user = userId;
 	image.save();
@@ -33,48 +99,3 @@ exports.image_post = function(req, res) {
 	})
 };
 
-exports.signup_post = function(req, res) {
-	var user = new User(
-		{
-			username: req.body.login,
-			password: req.body.password
-		}
-	);
-	User.findOne({ 'username': req.body.login })
-		.exec( function(err, found_user) {
-			if (err) { console.log("Error occurred!"); }
-
-			if (found_user) { console.log("Username already taken."); }
-
-			else {
-				user.save();
-			}
-		})
-	return;
-};
-
-exports.login_post = function(req, res) {
-	User.findOne({ 'username': req.body.login })
-		.exec( function(err, found_user) {
-			if (err) { console.log("Error occurred!"); }
-
-			if (found_user) {  
-				if (req.body.password === found_user.password) {
-					res.json({
-						confirmation: 'success',
-						data: found_user
-					})
-				}
-				else {
-					res.json({
-						confirmation: 'fail'
-					})
-				}
-			}
-
-			else {
-				console.log("Username is not in the system - please register");
-			}
-		})
-	return;
-};
